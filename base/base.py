42bf7a4e911653fd0694c521fa9b2c9e0a753351
grymyk <grymyk@gmail.com>
2020-02-03 16:24:42 +0200

    remove comments

--- base/base.py
+++ base/base.py
@@ -14,19 +14,15 @@ from OpenGL.GL import (
 
 from OpenGL.GLU import gluPerspective
 
-# ~ from octahedron import vertices, faces, edges, face_colors
-# ~ from cube import vertices, faces, edges, face_colors
-
-# ~ from coords import archimedean_spiral
 from shape import Shape
 
 def camera():
     pygame.init()
+
     display = (1280, 1260)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
 
     dZ = -50
     glTranslatef(0.0, 0.0, dZ)
